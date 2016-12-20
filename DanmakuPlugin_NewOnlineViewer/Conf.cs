9d1789cfeae56a1bec70c952c4afdbb35118c053
XZso3 <xzso3@outlook.com>
2016-12-21 07:56:14 +0800

    update initalize config

---
+++ DanmakuPlugin_NewOnlineViewer/Conf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DanmakuPlugin_NewOnlineViewer
+{
+    internal static partial class Conf
+    {
+        internal static readonly string PluginName          = "在线人数视窗";
+        internal static readonly string PluginAuthor        = "Const";
+        internal static readonly string PluginVersion       = "alpha-p1";
+        internal static readonly string PluginDescription   = "在桌面部署一个置顶窗口浏览当前在线观众人数";
+        internal static readonly string PluginContact       = "xzso3@outlook.com";
+        ///
+        /// 插件基本信息
+        /// 
+
+
+
+
+    }
+}
